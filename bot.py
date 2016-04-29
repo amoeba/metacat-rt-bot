b9af33a09a354fc5c630fa60b65f70810e87888b
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-04-28 16:23:18 -0800

    Finish RT integration

--- bot.py
+++ bot.py
@@ -13,13 +13,22 @@ import requests
 from dotenv import load_dotenv
 import rt
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# Dynamic variables
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 LASTFILE_PATH = os.environ.get("LASTFILE_PATH")
 MAX_ITEMS = int(os.environ.get("MAX_ITEMS"))
+BASE_URL = os.environ.get("BASE_URL")
 SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL")
 USERS = os.environ.get("USERS")
+RT_URL = os.environ.get("RT_URL")
+RT_USER = os.environ.get("RT_USER")
+RT_PASS = os.environ.get("RT_PASS")
+RT_TICKET_OWNER = os.environ.get("RT_TICKET_OWNER")
+
+# Hard-coded variables
+PID_STARTSWITH = "arctic-data."
+EML_FMT_ID = "eml://ecoinformatics.org/eml-2.1.1"
 
 
 def send_message(message):
@@ -44,6 +53,17 @@ def create_message(count, url):
                 "You can see more detail at {}.")
 
     message = template.format(USERS, count, objects, was, url_esc)
+
+    return message
+
+
+def create_tickets_message(tickets):
+    message = "The following tickets were just created:\n"
+
+    for ticket in tickets:
+        ticket_url = "{}/Ticket/Display.html?id={}".format(RT_URL, ticket)
+        line = "- {}\n".format(ticket_url)
+        message += line
 
     return message
 
@@ -81,6 +101,24 @@ def get_object_identifiers(doc):
     return [o.find('identifier').text for o in doc.findall("objectInfo")]
 
 
+def get_metadata(doc):
+    metadata = []
+
+    # Filter to EML 2.1.1 objects
+    for o in doc.findall("objectInfo"):
+        format_id = o.find('formatId').text
+        pid = o.find('identifier').text
+
+        if format_id == EML_FMT_ID and pid.startswith(PID_STARTSWITH):
+            metadata.append(o.find('identifier').text)
+
+    return metadata
+
+
+def now():
+    return datetime.datetime.utcnow().isoformat()
+
+
 def get_last_run():
     last_run = None
 
@@ -95,13 +133,38 @@ def get_last_run():
     return last_run
 
 
-def now():
-    return datetime.datetime.utcnow().isoformat()
+def save_last_run(to_date):
+    with open(os.path.join(os.path.dirname(__file__), LASTFILE_PATH), "w") as f:
+        f.write(to_date)
+
+
+def create_or_update_tickets(identifiers):
+    if len(identifiers) <= 0:
+        return None
+
+    tracker = rt.Rt("{}/REST/1.0/".format(RT_URL), RT_USER, RT_PASS)
+
+    if tracker.login() is False:
+        send_message("Hey @bryce, I failed to log into RT. Something's wrong!")
+        raise Exception("Failed to log in to RT.")
+
+    tickets = []
+
+    for identifier in identifiers:
+        ticket = ticket_find(tracker, identifier)
+
+        if ticket is None:
+            tickets.append(ticket_create(tracker, identifier))
+        else:
+            ticket_reply(tracker, ticket)
+
+    return tickets
 
 
 def ticket_find(tracker, pid):
-    ids = [t['id'] for t in tracker.search(Queue='arcticdata',
-                                           Subject__like=pid)]
+    title = 'Submission: {}'.format(pid)
+    results = tracker.search(Queue='arcticdata', Subject__like=title)
+    ids = [t['id'].replace('ticket/', '') for t in results]
 
     if len(ids) > 0:
         return ids[0]
@@ -110,13 +173,14 @@ def ticket_find(tracker, pid):
 
 
 def ticket_create(tracker, pid):
-    rt.create_ticket(Queue='arcticdata',
-                     Subject="{}".format(pid),
-                     Text=("This ticket was automatically created by the "
-                           "listobjects bot because the PID {} was created. "
-                           "See https://github.nceas.ucsb.edu/KNB/arctic-data/"
-                           "blob/master/docs/handling-submissions.md for more "
-                           "information on what to do.").format(pid))
+    tracker.create_ticket(Queue='arcticdata',
+                          Subject="New submission: {}".format(pid),
+                          Owner=RT_TICKET_OWNER,
+                          Text=("This ticket was automatically created by the "
+                                "listobjects bot because the PID {} was modified. "
+                                "See https://github.nceas.ucsb.edu/KNB/arctic-data/"
+                                "blob/master/docs/handling-submissions.md for more "
+                                "information on what to do.").format(pid))
 
 
 def ticket_reply(tracker, ticket_id):
@@ -137,6 +201,8 @@ def main():
 
     with open(os.path.join(os.path.dirname(__file__), LASTFILE_PATH), "w") as f:
         f.write(to_date)
+        tickets = create_or_update_tickets(get_metadata(doc))
+        send_message(create_tickets_message(tickets))
 
 
 if __name__ == "__main__":
