df6f669ef0185ee25b9740fc595f4fc7eafb2b02
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-05-12 10:53:22 -0800

    Make ticket creation message a one-liner
    This fixes tickets being created with only the first
    line of the message. I'm not sure if this is an error
    on my end or on the RT end.

--- bot.py
+++ bot.py
@@ -172,13 +172,7 @@ def ticket_create(tracker, pid):
 
 
 def create_ticket_text(pid):
-    template = """A new submission just came in.
-
-    View it here: https://arcticdata.io/catalog/#view/{}
-
-    This ticket was automatically created by the listobjects bot because the PID {} was created/modified.
-
-    See https://github.nceas.ucsb.edu/KNB/arctic-data/blob/master/docs/handling-submissions.md for more information on what to do.")"""
+    template = """A new submission just came in. View it here: https://arcticdata.io/catalog/#view/{}. This ticket was automatically created by the listobjects bot because the PID {} was created/modified. See https://github.nceas.ucsb.edu/KNB/arctic-data/blob/master/docs/handling-submissions.md for more information on what to do.")"""
 
     return template.format(pid, pid)
 
