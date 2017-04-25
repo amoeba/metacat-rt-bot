3701072c20370112b3db17fa7948ad3acf039e16
Bryce Mecum <petridish@gmail.com>
2017-04-25 11:49:15 -0800

    Update link to submission handling docs

--- bot.py
+++ bot.py
@@ -229,7 +229,7 @@ def ticket_create(pid):
 
 
 def create_ticket_text(pid):
-    template = """A new submission just came in. View it here: https://arcticdata.io/catalog/#view/{}. This ticket was automatically created by the submissions bot because the PID {} was created/modified. See https://github.nceas.ucsb.edu/KNB/arctic-data/blob/master/docs/handling-submissions.md for more information on what to do. If you aren't sure why this ticket was created, please see the README at https://github.nceas.ucsb.edu/KNB/submissions-bot.")"""
+    template = """A new submission or updated to an existing submission just came in. View it here: https://arcticdata.io/catalog/#view/{}. This ticket was automatically created by the submissions bot because the PID {} was created/modified. See https://github.nceas.ucsb.edu/KNB/arctic-data/blob/master/datateam/Dataset_Submission/Reviewing-Handling-Submissions.md for more information on what to do. If you aren't sure why this ticket was created, please see the README at https://github.nceas.ucsb.edu/KNB/submissions-bot.")"""
 
     return template.format(pid, pid)
 
